ceaaaa147a1a2bbb9e5531beaa3b7ccf6a5db7af
gamesa17 <104396970+gamesa17@users.noreply.github.com>
2022-09-19 17:05:12 +0300

    Add files via upload

---
+++ avl_three/avl_three.cpp
@@ -0,0 +1,167 @@
+﻿// avl_three.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+// Реализация узла
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
+
+int max(int a, int b);
+
+// Вычисление высоты
+int height(Node* N) {
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// Создание новой динамической структуры данных
+Node* newNode(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return (node);
+}
+
+// Поворот направо
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(height(y->left),
+        height(y->right)) +
+        1;
+    x->height = max(height(x->left),
+        height(x->right)) +
+        1;
+    return x;
+}
+
+// Поворт на лево
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(height(x->left),
+        height(x->right)) +
+        1;
+    y->height = max(height(y->left),
+        height(y->right)) +
+        1;
+    return y;
+}
+
+// Получение коэффициента баланса для каждого узла
+int getBalanceFactor(Node* N) {
+    if (N == NULL)
+        return 0;
+    return height(N->left) -
+        height(N->right);
+}
+
+// Ввод узла
+Node* insertNode(Node* node, int key) {
+    // Нахождение правильного местоположения для каждого узла
+    if (node == NULL)
+        return (newNode(key));
+    if (key < node->key)
+        node->left = insertNode(node->left, key);
+    else if (key > node->key)
+        node->right = insertNode(node->right, key);
+    else
+        return node;
+
+    // Обновление коэффициента баланса для каждого узла
+    // Балансировка дерева
+    node->height = 1 + max(height(node->left),
+        height(node->right));
+    int balanceFactor = getBalanceFactor(node);
+    if (balanceFactor > 1) {
+        if (key < node->left->key) {
+            return rightRotate(node);
+        }
+        else if (key > node->left->key) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+    }
+    if (balanceFactor < -1) {
+        if (key > node->right->key) {
+            return leftRotate(node);
+        }
+        else if (key < node->right->key) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+    }
+    return node;
+}
+
+// Узел с минимальным значением
+Node* nodeWithMimumValue(Node* node) {
+    Node* current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+// Вывод АВЛ дерева
+void printTree(Node* root, string indent, bool last) {
+    if (root != nullptr) {
+        cout << indent;
+        if (last) {
+            cout << "R----";
+            indent += "   ";
+        }
+        else {
+            cout << "L----";
+            indent += "|  ";
+        }
+        cout << root->key << endl;
+        printTree(root->left, indent, false);
+        printTree(root->right, indent, true);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    Node* root = NULL;
+    root = insertNode(root, 20);
+    root = insertNode(root, 15);
+    root = insertNode(root, 9);
+    root = insertNode(root, 18);
+    root = insertNode(root, 40);
+    root = insertNode(root, 35);
+    root = insertNode(root, 51);
+    root = insertNode(root, 27);
+    root = insertNode(root, 37);
+    root = insertNode(root, 36);
+    printTree(root, "", true);
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
